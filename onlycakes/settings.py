35fe599a2c68b22cf8b168dfac8daf76e882a1e7
username <jcaputo66@aol.com>
2023-02-03 14:59:30 -0800

    CSRF origins

--- onlycakes/settings.py
+++ onlycakes/settings.py
@@ -140,3 +140,5 @@ TAGGIT_CASE_INSENSITIVE=True
 
 import django_on_heroku
 django_on_heroku.settings(locals())
+
+CSRF_TRUSTED_ORIGINS = ['https://onlycakes.up.railway.app', 'http://onlycakes.up.railway.app']
