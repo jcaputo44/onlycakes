4064968b39b094f816e71b07a8abd10a8a5af530
username <jcaputo66@aol.com>
2023-02-03 14:29:32 -0800

    BASE_DIR

--- onlycakes/settings.py
+++ onlycakes/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / '/home/www/https://onlycakes.up.railway.app/static/'
 
 
 
